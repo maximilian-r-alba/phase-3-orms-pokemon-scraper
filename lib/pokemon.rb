7632dcce690d9c6db369b29969017080a999bfca
Maximilian Alba <albam476@gmail.com>
2023-03-06 18:22:24 -0500

    commit tree b91747e2d02c32f61f94a8b1247a225c9f6f65c6

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class Pokemon
+    attr_accessor :id , :name , :type , :db
+   
+    def initialize (id: , name: , type: , db:)
+        @id , @name , @type , @db = id , name , type , db
+    end
+
+    def self.save (name , type , db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES (? , ?)
+        SQL
+        
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id , db)
+        info = db.execute("SELECT * FROM pokemon WHERE id=? " , id)[0]
+        Pokemon.new(id: info[0], name:info[1], type:info[2], db: db)
+    end
+
 end
